20152c4b0efe65fafd75248374f37975c142da53
johnson <amencabnaamen@gmail.com>
2024-06-02 19:16:48 +0100

    Adding pages with react-Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './pages/home';
+import Html from './pages/html';
+import Css from './pages/css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1 > Sorrrrrrrry</h1>
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+    errorElement: <h1 > Sorrrrrrrry</h1>
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+    errorElement: <h1 > Sorrrrrrrry</h1>
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
