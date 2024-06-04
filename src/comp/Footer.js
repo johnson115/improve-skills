ee0a3efb22240377c00cde4fbb6262816d73ee52
johnson <amencabnaamen@gmail.com>
2024-06-04 13:49:46 +0100

    Adding the basic components in my project

---
+++ src/comp/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      Designed and developed by Amen allah Naamen
+      <span> 🧡</span>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,80 +1,24 @@
 
+import Header from '../comp/Header';
+import Footer from '../comp/Footer';
+
+
+
+
 const Home = () => {
   return (
     <>
-      <header className="hide-when-mobile">
-        <h1>Courses 4 Arab</h1>
-        <ul className="flex">
-          <li className="main-list">
-            <a className="main-link" href="#">
-              
-              HTML
-            </a>
-            <ul className="sub-ul">
-              <li>
-                <a href="">Full Course</a>
-              </li>
-              <li>
-                <a href="">Crash Course</a>
-              </li>
-              <li>
-                <a href="">learn in 1h</a>
-              </li>
-            </ul>
-          </li>
-          <li className="main-list">
-            <a className="main-link" href="#">
-              
-              CSS
-            </a>
-            <ul className="sub-ul">
-              <li>
-                <a href="">Full Course</a>
-              </li>
-              <li>
-                <a href="">CSS Examples</a>
-              </li>
-              <li className="mini-projects">
-                <a href="">mini projects&nbsp; + </a>
-                <ul className="sub-sub-ul">
-                  <li>
-                    <a href="">project 1</a>
-                  </li>
-                  <li>
-                    <a href="">project 2</a>
-                  </li>
-                  <li>
-                    <a href="">project 3</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="main-list">
-            <a className="main-link" href="#">
-              
-              JavaScript
-            </a>
-            <ul className="sub-ul sub-of-js">
-              <li>
-                <a href="">coming soon🔥</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </header>
-      <header className="show-when-mobile">
-        <h1>Courses 4 Arab</h1>
-        <label className="absolute" htmlFor="burger">
-          <i className="fas fa-bars" />
-        </label>
-        <input id="burger" type="checkbox" />
-      </header>
-    </>
+
+    <Header />   
+
+   
+    
+    <main>
+      Home Page
+    </main>
+    <Footer />
+  </>
   );
 }
 
 export default Home;
-
-
-
